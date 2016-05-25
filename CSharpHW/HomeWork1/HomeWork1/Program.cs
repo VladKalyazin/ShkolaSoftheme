d9393ea15063e7ae63ace06e7f04a8596e1ab2be
VladKalyazin <999vlad@gmail.com>
2016-05-25 04:25:32 +0300

    Add files via upload

---
+++ CSharpHW/HomeWork1/HomeWork1/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeWork1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string name = "VladKalyazin";
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.WriteLine(name);
+            Console.ReadKey();
+        }
+    }
+}
