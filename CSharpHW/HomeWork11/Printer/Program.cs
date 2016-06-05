77c78457b317ff2c5715dcf9a4173d7f5ef96853
VladKalyazin <999vlad@gmail.com>
2016-06-05 21:44:26 +0300

    11/13

---
+++ CSharpHW/HomeWork11/Printer/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Printer
+{
+    public class Printer
+    {
+        public virtual void Print(string arg)
+        {
+            Console.WriteLine(arg);
+        }
+    }
+
+    public class ColourPrinter : Printer
+    {
+        public override void Print(string arg)
+        {
+            base.Print(arg);
+        }
+
+        public void Print(string arg, ConsoleColor color) 
+        {
+            Console.ForegroundColor = color;
+            Print(arg);
+        }
+    }
+
+    public class PhotoPrinter : Printer
+    {
+        public override void Print(string arg)
+        {
+            base.Print(arg);
+        }
+
+        public void Print(object photo)
+        {
+            // do something with photo
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
---
+++ CSharpHW/HomeWork11/PrinterExtension/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Printer;
+
+namespace PrinterExtension
+{
+    public static class PrinterExtension
+    {
+        public static void PrintArray(this Printer.Printer printer, string[] args)
+        {
+            foreach (var str in args)
+                printer.Print(str);
+        }
+
+        public static void PrintArray(this Printer.ColourPrinter printer, string[] args, ConsoleColor color)
+        {
+            foreach (var str in args)
+                printer.Print(str, color);
+        }
+
+        public static void PrintArray(this Printer.PhotoPrinter printer, object[] photos)
+        {
+            foreach (var photo in photos)
+                printer.Print(photo);
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
