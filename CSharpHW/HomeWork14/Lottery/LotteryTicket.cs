5cf6067090f3d04b0e72a0dded572d0ee8307e75
VladKalyazin <999vlad@gmail.com>
2016-06-12 18:45:22 +0300

    indexers

---
+++ CSharpHW/HomeWork14/Lottery/LotteryTicket.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lottery
+{
+    public class LotteryTicket
+    {
+        public const int CountOfNumbers = 6;
+        public const int MinValue = 1;
+        public const int MaxValue = 9;
+
+        private List<int> _Numbers { get; set; } = new List<int>();
+
+        public LotteryTicket()
+        {
+            Random generator = new Random();
+            for (int i = 0; i < CountOfNumbers; i++)
+                _Numbers.Add(generator.Next(1, 9));
+        }
+
+        public int this[int key]
+        {
+            get
+            {
+                if (key < 0 || key >= CountOfNumbers)
+                    throw new IndexOutOfRangeException("Invalid index");
+                return _Numbers[key];
+            }
+        }
+    }
+}
---
+++ CSharpHW/HomeWork14/Lottery/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lottery
+{
+    public class Program
+    {
+        public static List<int> GetValues()
+        {
+            List<int> result = new List<int>();
+            Console.WriteLine($"Input {LotteryTicket.CountOfNumbers} integer values");
+            for (int i = 0; i < LotteryTicket.CountOfNumbers; i++)
+            {
+                try
+                {
+                    int value = Convert.ToInt32(Console.ReadLine());
+                    result.Add(value);
+                }
+                catch
+                {
+                    Console.WriteLine("Input error. Try again");
+                    return GetValues();
+                }
+            }
+
+            return result;
+        }
+
+        static void Main(string[] args)
+        {
+            var ticket = new LotteryTicket();
+            List<int> values = GetValues();
+            bool isCorrect = true;
+            for (int i = 0; i < LotteryTicket.CountOfNumbers; i++)
+            {
+                if (ticket[i] != values[i])
+                {
+                    isCorrect = false;
+                    break;
+                }
+            }
+            if (isCorrect)
+                Console.WriteLine("Congratulations");
+            else
+                Console.WriteLine("You lose");
+
+            Console.ReadKey();
+        }
+    }
+}
