699e67156899d8bb3eaebfefb14b41eae72963e3
1 <1@ZZIDEA500W10>
2016-06-13 13:12:42 +0300

    quiz2

---
+++ Algorithms/Quiz2/Task1/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task1
+{
+    class Program
+    {
+        public const int MinValue = 1;
+        public const int MaxValue = 1000;
+
+        public struct IntPair
+        {
+            public int A { get; set; }
+            public int B { get; set; }
+
+            public IntPair(int a, int b)
+            {
+                A = a;
+                B = b;
+            }
+        }
+
+        public static void PrintSolutions()
+        {
+            Dictionary<int, List<IntPair>> resultsTable = new Dictionary<int, List<IntPair>>();
+
+            for (int i = MinValue; i <= MaxValue; i++)
+            {
+                for (int j = i; j <= MaxValue; j++)
+                {
+                    int result = GetThirdDegree(i) + GetThirdDegree(j);
+                    if (!resultsTable.ContainsKey(result))
+                        resultsTable.Add(result, new List<IntPair>());
+                    resultsTable[result].Add(new IntPair(i, j));
+                }
+            }
+
+            foreach (var solutions in resultsTable)
+            {
+                if (solutions.Value.Count > 1)
+                {
+                    Console.WriteLine($"Result = {solutions.Key}");
+                    Console.WriteLine("Values:");
+                    foreach (var value in solutions.Value)
+                    {
+                        Console.WriteLine($"a = {value.A}; b = {value.B}");
+                    }
+                    Console.WriteLine();
+                }
+            }
+
+            Console.WriteLine("Done");
+        }
+
+        static int GetThirdDegree(int value) => value * value * value;
+
+        static void Main(string[] args)
+        {
+            PrintSolutions();
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Algorithms/Quiz2/Task3/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task3
+{
+    class Program
+    {
+        static string CompressString(string input)
+        {
+            string result = String.Empty;
+            char current = input[0];
+            int counter = 1;
+            for (int i = 1; i < input.Length; i++)
+            {
+                char c = input[i];
+                if (c != current)
+                {
+                    result += current;
+                    result += counter.ToString();
+                    counter = 1;
+                    current = c;
+                }
+                else
+                {
+                    counter++;
+                }
+            }
+            if (counter > 0)
+            {
+                result += current;
+                result += counter.ToString();
+            }
+
+            if (result.Length < input.Length)
+                return result;
+            return input;
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine(CompressString("aaaaabbbbcccc"));
+            Console.WriteLine(CompressString("abc"));
+            Console.WriteLine(CompressString("aaaaabc"));
+
+            Console.ReadKey();
+        }
+    }
+}
