04a43661278efbeca4e78c373120fe0bef02edd8
VladKalyazin <999vlad@gmail.com>
2016-06-26 19:43:13 +0300

    homework26(the same project)

---
+++ CSharpHW/HomeWork26/HomeWork25/Archiver.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.IO;
+using System.IO.Compression;
+
+namespace HomeWork25
+{
+    public static class TaskArchiver
+    {
+        public static void Archive(string path)
+        {
+            DirectoryInfo dir = new DirectoryInfo(path);
+            var files = dir.GetFiles();
+            foreach (var file in files)
+            {
+                if (file.Extension != ".zip")
+                {
+                    Task.Factory.StartNew(() =>
+                    {
+                        string archiveName = file.DirectoryName + '\\' + file.Name.Replace(file.Extension, file.Extension.Replace('.', '_')) + ".zip";
+                        using (ZipArchive archiver = ZipFile.Open(archiveName, ZipArchiveMode.Create))
+                        {
+                            Console.WriteLine($"Start achiving file \"{file.FullName}\"");
+                            archiver.CreateEntryFromFile(file.FullName, file.Name);
+                            //archiver.ExtractToDirectory(file.DirectoryName);
+                            Console.WriteLine($"End achiving file \"{file.FullName}\"");
+                        }
+                    });
+                }
+            }
+
+            foreach (var subDir in dir.GetDirectories())
+            {
+                Archive(subDir.FullName);
+            }
+
+        }
+
+        public static void Extract(string path)
+        {
+            DirectoryInfo dir = new DirectoryInfo(path);
+            var files = dir.GetFiles();
+            foreach (var file in files)
+            {
+                if (file.Extension == ".zip")
+                {
+                    Task.Factory.StartNew(() =>
+                    {
+                        using (ZipArchive archiver = ZipFile.Open(file.FullName, ZipArchiveMode.Read))
+                        {
+                            Console.WriteLine($"Start extracting file \"{file.FullName}\"");
+                            archiver.ExtractToDirectory(file.DirectoryName);
+                            Console.WriteLine($"End extracting file \"{file.FullName}\"");
+                        }
+                    });
+                }
+            }
+
+            foreach (var subDir in dir.GetDirectories())
+            {
+                Extract(subDir.FullName);
+            }
+
+        }
+
+    }
+
+}
---
+++ CSharpHW/HomeWork26/HomeWork25/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace HomeWork25
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //TaskArchiver.Archive("C:\\Users\\1user\\Documents\\softheme\\25_Multithreading");
+            TaskArchiver.Extract("C:\\Users\\1user\\Documents\\softheme\\25_Multithreading");
+
+            Console.ReadKey();
+        }
+    }
+}
